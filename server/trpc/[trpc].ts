348cae52b0e8d56298dedba7f63b816f02f3a9d7
AhmedKhchai <a.khchai@gmail.com>
2023-05-10 10:31:27 +0200

    frontend client

---
+++ server/trpc/[trpc].ts
@@ -0,0 +1,9 @@
+import { createNuxtApiHandler } from 'trpc-nuxt';
+import { appRouter } from '@/server/trpc/routers';
+
+// export API handler
+export default createNuxtApiHandler({
+    router: appRouter,
+    createContext: () => ({})
+});
+
